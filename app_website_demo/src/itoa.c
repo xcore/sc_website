126039cd15a5eaf78ae6d67c1d222a9da127cfb8
David Lacey <david.lacey@xmos.com>
2012-09-10 19:52:13 +0100

    Added missing files

---
+++ app_website_demo/src/itoa.c
@@ -0,0 +1,31 @@
+static void reverse_array(char buf[], unsigned size)
+{
+  int begin = 0;
+  int end = size - 1;
+  int tmp;
+  for (;begin < end;begin++,end--) {
+    tmp = buf[begin];
+    buf[begin] = buf[end];
+    buf[end] = tmp;
+  }
+}
+
+
+int itoa(unsigned int n, char buf[], int base, int fill)
+{ static const char digits[] = "0123456789ABCDEF";
+  unsigned int i = 0;
+  while (n > 0) {
+    unsigned int next = n / base;
+    unsigned int cur  = n % base;
+    buf[i] = digits[cur];
+    i += 1;
+    fill--;
+    n = next;
+  }
+  for (;fill > 0;fill--) {    
+    buf[i] = '0';
+    i++;
+  }
+  reverse_array(buf, i);
+  return i;
+}
