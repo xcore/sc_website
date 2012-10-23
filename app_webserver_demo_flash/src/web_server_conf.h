4d02ded93b2afb1a727c13fb34887605d9334a0f
David Lacey <david.lacey@xmos.com>
2012-10-23 11:16:58 +0100

    Fix app config files

--- app_webserver_demo_flash/src/web_server_conf.h
+++ app_webserver_demo_flash/src/web_server_conf.h
@@ -8,4 +8,8 @@
 #define WEB_SERVER_SEPARATE_FLASH_TASK 1
 #endif
 
+// Set the function to run after rendering a page
+#define WEB_SERVER_POST_RENDER_FUNCTION post_page_render_increment_counter
+
+
 #include "web_page_functions.h"
